62b72cf63ec39fb90841dc03ac88c7d1c5803f13
jonnywintech <jonnywintech@gmail.com>
2023-07-28 07:36:27 +0200

    unfinished add item to cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import { useRef, useState } from 'react';
 
 const MealItemForm = (props) => {
-  return (
-    <form className={classes.form}>
-      <Input
-        label='Amount'
-        input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
-        }}
-      />
-      <button>+ Add</button>
-    </form>
-  );
-};
+  const amountInputRef = useRef();
+
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const eneteredAmount = +enteredAmount; // convert string to number
+
+    if (enteredAmount.trim() === 0 || enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber)
+  }
+    return (
+      <form className={classes.form} onSubmit={submitHandler}>
+        <Input
+          ref={amountInputRef}
+          label='Amount'
+          input={{
+            id: 'amount_' + props.id,
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1',
+          }}
+        />
+        <button>+ Add</button>
+        {!amountIsValid && <p>Enter valid amount (1-5)</p>}
+      </form>
+    );
+  };
 
 export default MealItemForm;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,21 +1,49 @@
-import React from 'react'
-import CartContext from './cart-context'
+import React from 'react';
+import { useReducer } from 'react';
+import CartContext from './cart-context';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const newTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+      return{
+        items: updatedItems,
+        totalAmount: newTotalAmount,
+      }
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState,
+  );
 
-    const addItemToCartHandler = (props) => {};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
 
-    const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler,
-    };
-  return <CartContext.Provider value={cartContext}>
-    {props.children}
-  </CartContext.Provider>
-}
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
-export default CartProvider
+export default CartProvider;
