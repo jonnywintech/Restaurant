e6ae6e3f9bdc197a468d8733da383b54a16844fb
jonnywintech <jonnywintech@gmail.com>
2023-07-30 22:41:11 +0200

    added animation for final touch

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect} from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = React.useState(false);
+  
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  const { items } = cartCtx;
+
+  useEffect(()=>{
+
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return ()=>{
+      clearTimeout(timer)
+    }
+  },[items]);
+
   return (
-    <button className={classes['button']} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes['icon']}>
         <CartIcon />
       </span>
